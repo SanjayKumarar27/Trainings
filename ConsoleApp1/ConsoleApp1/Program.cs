cda037eb07aa5decbc75d8493344ad3d9d540d67
Sanjaykumarar27 <sanjaykumarar3@gmail.com>
2025-08-29 10:11:12 +0530

    C# for loop

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,79 @@
 ﻿namespace ConsoleApp1
 {
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int[] arr =new int[4];
+            //int input = 0;
+            //for(int i=0;i<4;i++)
+            //{
+            //    int.TryParse(Console.ReadLine(), out input);
+            //    arr[i] =input;
+            //}
+            //Console.WriteLine("-----------------------------");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //take 5 number from user and display 
+            //1.sum
+            //2. min
+            //3.max
+            //4.Avg
+            //Search a number
+            //Console.WriteLine("Enter 5 numbers");
+            //int[] arr = new int[5];
+            //int input = 0;
+            //int sum = 0;
+            //int minvalue=int.MaxValue;
+            //int maxvalue=int.MinValue;
+            //float avg = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    int.TryParse(Console.ReadLine(), out input);
+            //    sum+= input;
+            //    minvalue = int.Min(input, minvalue);
+            //    maxvalue = int.Max(input, maxvalue);
+
+            //    arr[i] = input;
+            //}
+            //Console.WriteLine("Sum Is :{0}", sum);
+            //Console.WriteLine("Min is : {0}", minvalue);
+            //Console.WriteLine("Max is : {0}",maxvalue);
+
+            //avg=(sum/(float)arr.Length);
+            //Console.WriteLine("Avg is :{0}",avg);
+
+            //Console.WriteLine("Search any number :");
+            //int serach;
+            //int.TryParse(Console.ReadLine(), out serach);
+            //bool ispresent = false;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if(arr[i] == serach)
+            //    {
+            //        ispresent = true;
+
+            //    }
+            //}
+            //if (ispresent)
+            //{
+            //    Console.WriteLine("The Search value is Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Search value is Not Found");
+            //}
+            ////any operation byte ,short int will result in int like byte+byte=int and short+short=int
+            //Console.WriteLine("The elements in the array are ---------");
+            //foreach (int i in arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
         }
     }
-}
+}   
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b6c3c76a83d97a3f550ae583713e28076e62eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
